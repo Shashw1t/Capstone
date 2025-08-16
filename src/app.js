0d6d1c30286a190ddb62f2f07c3e05ba308bb650
Shashw1t <iamsoranic@gmail.com>
2025-08-16 16:31:10 +0530

    capstone basic setup

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(express.json());
+
+app.get('/', (req,res) => {
+    res.send(`ITR filing service API is running on port ${process.env.PORT}`);
+});
+
+module.exports = app;
